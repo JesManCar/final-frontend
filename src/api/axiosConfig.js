35b5d464cdb0fc4bbddda3fd8f8b1f6d047e92ba
JesManCar <jesmancar99@gmail.com>
2025-07-21 23:23:50 +0200

    fix: :bug: fix axios url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const API = axios.create({
-  baseURL: 'https://final-project-backend.onrender.com',
+  baseURL: 'https://final-backend-fzec.onrender.com',
   withCredentials: true, 
 });
 
