97e658a6dfce57693ce177141c9f4ef99f95c96b
JesManCar <jesmancar99@gmail.com>
2025-07-21 22:11:58 +0200

    fix: fix url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,26 +2,14 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const API = axios.create({
-  /*baseURL: 'http://localhost:3001/',*/
-  baseURL: 'https://final-project-backend.onrender.com/',
-  withCredentials: true, // permite envío de cookies
-
+  baseURL: 'https://final-project-backend.onrender.com',
+  withCredentials: true, 
 });
-
-/*const API_FormType = axios.create({
-  baseURL: 'http://localhost:3001/',
-  headers: {
-    'Content-Type': 'multipart/form-data', // para enviar archivos
-    withCredentials: true, // permite envío de cookies
-  }
-
-});*/
 
 API.interceptors.request.use(config => {
   const token = Cookies.get('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   config.headers['Content-Type'] = 'application/json'; // asegura que el tipo de contenido sea JSON
-  //config.headers['Content-Type'] = 'multipart/form-data'; // asegura que el tipo de contenido sea multipart/form-data
   return config;
 });
 
