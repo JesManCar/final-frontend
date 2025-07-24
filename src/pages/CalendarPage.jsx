81cdeb99a74c1fb3ec21ee58bd1b2d2ed1fb7b1a
JesManCar <jesmancar99@gmail.com>
2025-07-24 19:47:52 +0200

    style(Calendar): :art: Refactor CalendarPage styles

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -17,10 +17,10 @@ export default function CalendarPage() {
     <>
         <Nav />
         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '80vh', width: '80%', position: 'relative', left: '200px'}}>
-            <div style={{ flex: 1, height: '75vh', width: '75%', marginLeft: "10px", marginRight: "30px" }}>
+            <div style={{ height: '75vh', width: '75%', marginLeft: "10px", marginRight: "30px" }}>
             <Calendar date={selectedDate} handleDateChange={handleDateChange}  />
             </div>
-            <div style={{ flex: 2 ,width: '20%' }}>
+            <div style={{width: '20%' }}>
             <CalendarForm date={selectedDate}  />
             </div>
         </div>
